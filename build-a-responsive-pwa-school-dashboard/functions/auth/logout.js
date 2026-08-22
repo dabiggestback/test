bc346133af5c751fec991cad71f830fb1c050c67
AJ <ajbelle@proton.me>
2026-08-22 13:21:55 -0400

    Delete build-a-responsive-pwa-school-dashboard directory

--- build-a-responsive-pwa-school-dashboard/functions/auth/logout.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet() {
-  return new Response(null, {
-    status: 302,
-    headers: {
-      Location: '/#classroom',
-      'Set-Cookie': 'knoll_classroom_session=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Lax'
-    }
-  });
-}
